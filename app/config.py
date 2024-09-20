4ed83749530e50df682fe3ef1f5c590d62944ea3
Venkatasai dholendar reddy <130916331+dholendar27@users.noreply.github.com>
2024-09-20 10:11:15 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',"postgresql://username:password@localhost/db-name")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-JWT_SECRET_KEY = "ZVutz1gJLyaTdowLNha-9Q"
+JWT_SECRET_KEY = "secret-key"
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
